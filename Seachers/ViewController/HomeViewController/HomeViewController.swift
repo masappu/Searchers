008b5f1238fd3c12520ed08c74fa9f677e5f148d
西林遼太郎 <nishibayashiryoutarou@nishibayashiryoutarous-MacBook-Air.local>
2022-01-17 23:04:22 +0900

    pod追加のためcommit

--- Seachers/ViewController/HomeViewController/HomeViewController.swift
+++ Seachers/ViewController/HomeViewController/HomeViewController.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class HomeViewController: UIViewController {
 
+    @IBOutlet weak var NetShoppingButtom: UIButton!
+    @IBOutlet weak var GourmandButtom: UIButton!
+    @IBOutlet weak var TravelButtom: UIButton!
+    @IBOutlet weak var FavoriteButtom: UIButton!
+    
+    @IBOutlet weak var BackgroundImage: UIImageView!
+    
+    let image = UIImage(named: "")
+    let a = [UIImage(named: "")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        BackgroundImage.image = newImage
+        let fade = CATransition()
+        fade.duration = 1
+        fade.timingFunction = CAMediaTimingFunction(name: .default)
+        fade.type = CATransitionType.fade
+        BackgroundImage.layer.add(fade, forKey: "fade")
+        
+        NetShoppingButtom.layer.cornerRadius = 20
+        
+        GourmandButtom.layer.cornerRadius = 20
+        
+        TravelButtom.layer.cornerRadius = 20
+        
+        FavoriteButtom.layer.borderWidth = 1
+        FavoriteButtom.layer.cornerRadius = 10
+        
     }
 
     
