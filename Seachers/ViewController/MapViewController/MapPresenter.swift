d802de43c3e20d01e7ed470838a445522b707285
DaigoKondo <daigo.soccer.0106@icloud.com>
2022-01-19 19:42:27 +0900

    presenter修正中

--- Seachers/ViewController/MapViewController/MapPresenter.swift
+++ Seachers/ViewController/MapViewController/MapPresenter.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import GoogleMaps
 
 
 protocol MapPresenterInput {
@@ -13,7 +14,10 @@ protocol MapPresenterInput {
     func loadMap(gourmandSearchData:GourmandSearchDataModel)
     func reloadMap(gourmandSearchData:GourmandSearchDataModel,rangeCount:Int)
     func configureSubViews()
+    func requestScrollViewDidEndDecelerating(x:Double,width:Double)
+    func requestMapViewDidTap(marker:GMSMarker)
     var shopDataArray: [ShopDataDic]? {get set}
+    var markers: [GMSMarker]? {get set}
     
 }
 
@@ -24,11 +28,15 @@ protocol MapPresenterOutput {
     func setUpCollectionView()
     func setUpPickerView()
     func setUpSearchBar()
+    func responseScrollViewDidEndDecelerating(marker: GMSMarker)
+    func responseMapViewDidTap(marker: GMSMarker,index: Int)
     
 }
 
 class MapPresenter: MapPresenterInput{
+
     
+    var markers: [GMSMarker]?
     var shopDataArray: [ShopDataDic]?
     
     private var view: MapPresenterOutput!
@@ -41,6 +49,18 @@ class MapPresenter: MapPresenterInput{
         self.gourmandAPIModel = gourmandAPIModel
         self.travelAPIModel = TravelAPIModel()
     }
+    
+    func requestScrollViewDidEndDecelerating(x:Double,width:Double) {
+        let indexCount = x / width
+        let marker = markers![Int(indexCount)]
+        self.view.responseScrollViewDidEndDecelerating(marker: marker)
+    }
+    
+    func requestMapViewDidTap(marker:GMSMarker) {
+        let index = shopDataArray?.firstIndex(where: { $0.key == marker.title })
+        self.view.responseMapViewDidTap(marker: marker,index: index!)
+    }
+    
     
     func loadMap(gourmandSearchData:GourmandSearchDataModel) {
         self.view.setUpLocationManager()
@@ -63,8 +83,25 @@ class MapPresenter: MapPresenterInput{
 extension MapPresenter: GourmandAPIOutput{
     
     func resultAPIData(shopDataArray: [ShopDataDic], idoValue: Double, keidoValue: Double) {
+        
         self.shopDataArray = shopDataArray
         self.view.setUpMap(idoValue:idoValue,keidoValue:keidoValue)
+        for shopDataDic in shopDataArray{
+            makeMarker(shopData: shopDataDic.value!)
+        }
+    }
+    
+    func makeMarker(shopData:ShopData) {
+        let latitude = shopData.latitude!
+        let longitude = shopData.longitude!
+        let title = shopData.name!
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(latitude,longitude)
+        marker.appearAnimation = GMSMarkerAnimation.pop
+        marker.title = "\(title)"
+        marker.snippet = shopData.smallAreaName! + "/" + shopData.genreName!
+        markers!.append(marker)
     }
     
 }
