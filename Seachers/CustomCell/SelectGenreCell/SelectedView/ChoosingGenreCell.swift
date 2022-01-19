1246f48329374aa9540f21726a70215c250f227e
masappu <njzzmmi6tn@icloud.com>
2022-01-19 19:47:29 +0900

    ChoosingGenreCellの追加

---
+++ Seachers/CustomCell/SelectGenreCell/SelectedView/ChoosingGenreCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChoosingGenreCell.swift
+//  Seachers
+//
+//  Created by 山口誠士 on 2022/01/19.
+//
+
+import UIKit
+
+class ChoosingGenreCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
