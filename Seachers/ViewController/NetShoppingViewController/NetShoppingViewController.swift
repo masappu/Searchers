1d4ab89765a46430c2b93598d2f04ccc754683fb
西林遼太郎 <nishibayashiryoutarou@nishibayashiryoutarous-MacBook-Air.local>
2022-01-19 14:29:27 +0900

    Homeviewcontorllerを編集

--- Seachers/ViewController/NetShoppingViewController/NetShoppingViewController.swift
+++ Seachers/ViewController/NetShoppingViewController/NetShoppingViewController.swift
@@ -7,15 +7,66 @@
 
 import UIKit
 
-class NetShoppingViewController: UIViewController {
+class NetShoppingViewController: UIViewController,UISearchBarDelegate {
 
+    @IBOutlet weak var Label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setSearchBar()
 
         // Do any additional setup after loading the view.
     }
     
-
+    //検索バーの設置
+    func setSearchBar() {
+        // NavigationBarにSearchBarをセット
+        if let navigationBarFrame = self.navigationController?.navigationBar.bounds {
+            //NavigationBarに適したサイズの検索バーを設置
+            let searchBar: UISearchBar = UISearchBar(frame: navigationBarFrame)
+            //デリゲート
+            searchBar.delegate = self
+            //プレースホルダー
+            searchBar.placeholder = "ユーザーを検索"
+            //検索バーのスタイル
+            searchBar.autocapitalizationType = UITextAutocapitalizationType.none
+            //NavigationTitleが置かれる場所に検索バーを設置
+            navigationItem.titleView = searchBar
+            //NavigationTitleのサイズを検索バーと同じにする
+            navigationItem.titleView?.frame = searchBar.frame
+        }
+    }
+    
+    //検索バーで入力する時
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        //キャンセルボタンを表示
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    //検索バーのキャンセルがタップされた時
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        //キャンセルボタンを非表示
+        searchBar.showsCancelButton = false
+        //キーボードを閉じる
+        searchBar.resignFirstResponder()
+    }
+    
+    
+    //検索バーでEnterが押された時
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        //Labelに入力した値を設定
+        Label.text = searchBar.text as! String
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
+        
+        
+    }
     /*
     // MARK: - Navigation
 
