f38702a11b1847409663d48a73605840ca420d7a
masappu <njzzmmi6tn@icloud.com>
2022-01-22 01:26:21 +0900

    nonSelectGenreCellを追加

---
+++ Seachers/CustomCell/SelectGenreCell/nonSelectGenreCell.swift
@@ -0,0 +1,23 @@
+//
+//  nonSelectGenreCell.swift
+//  Seachers
+//
+//  Created by 山口誠士 on 2022/01/22.
+//
+
+import UIKit
+
+class nonSelectGenreCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
