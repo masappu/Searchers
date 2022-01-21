1f977185d28ab50a90fb91f1f2235bf3110494b9
DaigoKondo <daigo.soccer.0106@icloud.com>
2022-01-21 20:36:03 +0900

    セルのタイプ分け、webView実装中

--- Seachers/ViewController/MapViewController/MapPresenter.swift
+++ Seachers/ViewController/MapViewController/MapPresenter.swift
@@ -15,7 +15,7 @@ protocol MapPresenterInput {
     func loadMap(gourmandSearchData:GourmandSearchDataModel)
     func reloadMap(gourmandSearchData:GourmandSearchDataModel,rangeCount:Int)
     func configureSubViews()
-    func requestJudgementVc()
+//    func requestJudgementVc()
     func requestScrollViewDidEndDecelerating(x:Double,width:Double)
     func requestMapViewDidTap(marker:GMSMarker)
     func requestDoneButtonOfCategory(text: String)
@@ -35,7 +35,7 @@ protocol MapPresenterOutput {
     func setUpCollectionView()
     func setUpPickerView()
     func setUpSearchBar()
-    func responseJudgementVc()
+//    func responseJudgementVc()
     func responseScrollViewDidEndDecelerating(marker: GMSMarker)
     func responseMapViewDidTap(marker: GMSMarker,index: Int)
     func responseDoneButtonOfCategory(rangeCount:Int)
@@ -63,11 +63,11 @@ class MapPresenter: MapPresenterInput{
         self.gourmandAPIModel = gourmandAPIModel
         self.travelAPIModel = TravelAPIModel()
     }
-    
-    func requestJudgementVc() {
-        self.view.responseJudgementVc()
-    }
-    
+//    
+//    func requestJudgementVc() {
+//        self.view.responseJudgementVc()
+//    }
+//    
     func requestScrollViewDidEndDecelerating(x:Double,width:Double) {
         let indexCount = x / width
         let marker = markers[Int(indexCount)]
