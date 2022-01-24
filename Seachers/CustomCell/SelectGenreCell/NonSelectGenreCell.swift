bbf828a1ec46e70a51aad51c342ab21a6811eb91
masappu <91421375+masappu@users.noreply.github.com>
2022-01-24 14:35:17 +0900

    Update NonSelectGenreCell.swift

--- Seachers/CustomCell/SelectGenreCell/NonSelectGenreCell.swift
+++ Seachers/CustomCell/SelectGenreCell/NonSelectGenreCell.swift
@@ -17,7 +17,6 @@ class NonSelectGenreCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        let goumandVC = GourmandSearchViewController()
         // Configure the view for the selected state
     }
     
