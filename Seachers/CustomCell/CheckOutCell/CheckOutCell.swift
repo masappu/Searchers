faea1a91aa01dfc84ef61d968706b4ff1c622df0
tokouyuuki <tttyuki1050@gmail.com>
2022-01-26 01:51:49 +0900

    カスタムセルのファイルを追加しました

---
+++ Seachers/CustomCell/CheckOutCell/CheckOutCell.swift
@@ -0,0 +1,23 @@
+//
+//  CheckOutCell.swift
+//  Seachers
+//
+//  Created by 都甲裕希 on 2022/01/26.
+//
+
+import UIKit
+
+class CheckOutCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
