ff3a17f16661fc4a3619c11aad758505d240a915
tokouyuuki <tttyuki1050@gmail.com>
2022-01-26 02:35:30 +0900

    カスタムセルに追記

--- Seachers/CustomCell/CheckInCell/CheckInCell.swift
+++ Seachers/CustomCell/CheckInCell/CheckInCell.swift
@@ -8,16 +8,25 @@
 import UIKit
 
 class CheckInCell: UITableViewCell {
-
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var containerView: NSLayoutConstraint!
+    
+    static let compressedHeight:CGFloat = 55
+    static let expandedHeight:CGFloat = 150
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        containerView.constant = CheckInCell.compressedHeight
+        datePicker.isHidden = true
+        datePicker.alpha = 0
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
