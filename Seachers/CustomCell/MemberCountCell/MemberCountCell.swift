a59a7e2453d890a504025c07fdcc4fc99ed53280
masappu <njzzmmi6tn@icloud.com>
2022-01-27 17:13:22 +0900

    TravelAPIModelの実装（実装中）

--- Seachers/CustomCell/MemberCountCell/MemberCountCell.swift
+++ Seachers/CustomCell/MemberCountCell/MemberCountCell.swift
@@ -18,11 +18,13 @@ class MemberCountCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        self.plusButton.addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
+        self.plusButton.addTarget(self, action: #selector(touch(_:)), for: .touchDown)
         self.plusButton.addTarget(self, action: #selector(touchUpOutside(_:)), for: .touchUpOutside)
+        self.plusButton.addTarget(self, action: #selector(touch(_:)), for: .allTouchEvents)
         
-        self.minusButton.addTarget(self, action: #selector(touchDown(_:)), for: .touchDown)
+        self.minusButton.addTarget(self, action: #selector(touch(_:)), for: .touchDown)
         self.minusButton.addTarget(self, action: #selector(touchUpOutside(_:)), for: .touchUpOutside)
+        self.minusButton.addTarget(self, action: #selector(touch(_:)), for: .allTouchEvents)
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -31,7 +33,7 @@ class MemberCountCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    @objc func touchDown(_ sender:UIButton){
+    @objc func touch(_ sender:UIButton){
         self.buttonAnimat.startAnimation(sender: sender)
     }
 
