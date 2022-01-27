4db12647ec374a61946d07f01e6e7b6220b2ceb9
tokouyuuki <tttyuki1050@gmail.com>
2022-01-27 15:57:24 +0900

    TravelSearchDataModel作成、RoomAndMemberCellのファイル名変更、TravelSearchViewControllerフォルダの中身を追記

--- Seachers/CustomCell/MemberCountOfTravelCell/MemberCountOfTravelCell.swift
+++ Seachers/CustomCell/MemberCountOfTravelCell/MemberCountOfTravelCell.swift
@@ -8,16 +8,19 @@
 import UIKit
 
 class MemberCountOfTravelCell: UITableViewCell {
-
+    
+    @IBOutlet weak var memberCountLabel: UILabel!
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
     
 }
